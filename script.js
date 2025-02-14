f9bbe815c78200e1154ff0e5b5f7236f841edde0
Demo <demo@gmail.com>
2025-02-14 03:12:36 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const version = 7
+const version = 8
 document.getElementById("version").innerText = version;
 let originalData = null;
 
@@ -48,6 +48,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "sps_max_sub_layers_minus1", value: data[6] & 0x07 });
         fields.push({ name: "sps_temporal_id_nesting_flag", value: (data[7] >> 7) & 0x01 });
         fields.push({ name: "sps_separate_colour_plane_flag", value: (data[7] >> 6) & 0x01 });
+        fields.push({ name: "sps_pic_width_in_luma_samples", value: ((data[12] & 0x03) << 16) | ((data[13] & 0xFF) << 8) | data[14] });
     } else if (nalType === 34) {
         fields.push({ name: "pps_pic_parameter_set_id", value: data[5] & 0x3F });
         fields.push({ name: "pps_seq_parameter_set_id", value: data[6] & 0x1F });
