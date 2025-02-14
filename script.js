83a6ee0110ab68e4439e0dbd1922149c2f1d6fc2
Demo <demo@gmail.com>
2025-02-14 01:17:26 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // H.265 stream parser and editor
 
 // Specify the H.265 (HEVC) version supported by the script
-const version = 11
+const version = 12
 document.getElementById("version").innerText = version;
 
 let originalData = null;
@@ -50,6 +50,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "vps_max_layers_minus1", value: (data[8] >> 3) & 0x1F });
         fields.push({ name: "vps_max_sub_layers_minus1", value: data[8] & 0x07 });
         fields.push({ name: "vps_max_num_reorder_pics", value: (data[9] >> 5) & 0x07 });
+        fields.push({ name: "vps_max_latency_increase_plus1", value: data[9] & 0x1F });
     } else if (nalType === 33) {
         fields.push({ name: "sps_seq_parameter_set_id", value: data[5] & 0x1F });
         fields.push({ name: "sps_max_sub_layers_minus1", value: (data[6] >> 5) & 0x07 });
