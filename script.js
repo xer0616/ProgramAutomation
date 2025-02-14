817ae4db2e3a767eba8e638d1174edc3b50d8ed5
Demo <demo@gmail.com>
2025-02-14 01:36:02 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Specify the H.265 (HEVC) version supported by the script
-const version = 31
+const version = 32
 document.getElementById("version").innerText = version;
 
 let originalData = null;
@@ -57,6 +57,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "vps_max_dec_pic_buffering_minus1", value: (data[12] + (data[13] << 8)) >> 1 });
         fields.push({ name: "vps_num_units_in_tick", value: data[14] });
         fields.push({ name: "vps_num_ticks_poc_diff_one_minus1", value: data[15] & 0x3F });
+        fields.push({ name: "vps_num_hrd_parameters", value: data[15] >> 6 });
     } else if (nalType === 33) {
         fields.push({ name: "sps_seq_parameter_set_id", value: data[5] & 0x1F });
         fields.push({ name: "sps_max_sub_layers_minus1", value: (data[6] >> 5) & 0x07 });
