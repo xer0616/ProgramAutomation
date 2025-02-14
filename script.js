a2800e3bae363397916db0b137be3856151b421f
Demo <demo@gmail.com>
2025-02-14 02:44:46 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Specify the H.265 (HEVC) version supported by the script
-const version = 18
+const version = 19
 document.getElementById("version").innerText = version;
 
 let originalData = null;
@@ -73,7 +73,8 @@ function extractFields(nalType, data) {
         fields.push({ name: "sps_pic_width_in_luma_samples", value: data[9] + (data[10] << 8) + (data[11] << 16) + (data[12] << 24) });
         fields.push({ name: "sps_pic_height_in_luma_samples", value: data[13] + (data[14] << 8) });
         fields.push({ name: "sps_poc_width_in_luma_samples", value: data[15] + (data[16] << 8) });
-        fields.push({ name: "sps_separate_colour_plane_flag", value: (data[17] >> 7) & 0x01 });
+        fields.push({ name: "sps_conformance_window_flag", value: (data[17] >> 7) & 0x01 });
+        fields.push({ name: "sps_separate_colour_plane_flag", value: (data[17] >> 6) & 0x01 });
     } else if (nalType === 34) {
         fields.push({ name: "pps_pic_parameter_set_id", value: data[4] & 0x3F });
         fields.push({ name: "pps_seq_parameter_set_id", value: data[6] & 0x1F });
