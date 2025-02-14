6d2c63544c7eea4f46bb3519aa8fc83a6a52e208
Demo <demo@gmail.com>
2025-02-14 01:26:28 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Specify the H.265 (HEVC) version supported by the script
-const version = 20
+const version = 21
 document.getElementById("version").innerText = version;
 
 let originalData = null;
@@ -43,7 +43,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "vps_video_parameter_set_id", value: data[4] & 0x3F });
         fields.push({ name: "vps_reserved_three_2bits", value: (data[4] >> 6) & 0x03 });
         fields.push({ name: "vps_reserved_0xffff_16bits", value: data[5] + (data[6] << 8) });
-        fields.push({ name: "vps_sub_layer_ordering_info_present_flag", value: (data[7] >> 7) & 0x01 }); // Add the missing field
+        fields.push({ name: "vps_sub_layer_ordering_info_present_flag", value: (data[7] >> 7) & 0x01 });
         fields.push({ name: "vps_temporal_id_nesting_flag", value: (data[7] >> 6) & 0x01 });
         fields.push({ name: "vps_max_layers_minus1", value: (data[8] >> 3) & 0x1F });
         fields.push({ name: "vps_max_sub_layers_minus1", value: data[8] & 0x07 });
@@ -51,6 +51,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "vps_max_num_reorder_pics", value: (data[9] >> 0) & 0x0F });
         fields.push({ name: "vps_max_latency_increase_plus1", value: data[10] });
         fields.push({ name: "vps_max_layer_id", value: data[11] });
+        fields.push({ name: "vps_max_dec_pic_buffering_minus1", value: (data[12] + (data[13] << 8)) >> 1 }); // Add the missing field
     } else if (nalType === 33) {
         fields.push({ name: "sps_seq_parameter_set_id", value: data[5] & 0x1F });
         fields.push({ name: "sps_max_sub_layers_minus1", value: (data[6] >> 5) & 0x07 });
