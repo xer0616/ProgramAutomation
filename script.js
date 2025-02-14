b5352d4053063dfcc565cc19feb89bae0eed32ef
Demo <demo@gmail.com>
2025-02-14 01:32:29 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Specify the H.265 (HEVC) version supported by the script
-const version = 27
+const version = 28
 document.getElementById("version").innerText = version;
 
 let originalData = null;
@@ -53,6 +53,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "vps_max_layer_id", value: data[11] });
         fields.push({ name: "vps_layer_id_included_flag", value: (data[12] >> 7) & 0x01 });
         fields.push({ name: "vps_max_dec_pic_buffering_minus1", value: (data[12] + (data[13] << 8)) >> 1 });
+        fields.push({ name: "vps_num_units_in_tick", value: data[14] });
     } else if (nalType === 33) {
         fields.push({ name: "sps_seq_parameter_set_id", value: data[5] & 0x1F });
         fields.push({ name: "sps_max_sub_layers_minus1", value: (data[6] >> 5) & 0x07 });
@@ -84,4 +85,4 @@ document.getElementById("downloadBtn").addEventListener("click", function() {
 
 function modifyStream() {
     return originalData || new Uint8Array([]);
-} 
+}
