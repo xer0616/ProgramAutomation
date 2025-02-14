951732da90aede025a510128c032b6b93c7f35bb
Demo <demo@gmail.com>
2025-02-14 01:27:18 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Specify the H.265 (HEVC) version supported by the script
-const version = 21
+const version = 22
 document.getElementById("version").innerText = version;
 
 let originalData = null;
@@ -48,10 +48,11 @@ function extractFields(nalType, data) {
         fields.push({ name: "vps_max_layers_minus1", value: (data[8] >> 3) & 0x1F });
         fields.push({ name: "vps_max_sub_layers_minus1", value: data[8] & 0x07 });
         fields.push({ name: "vps_max_dpb_size", value: (data[9] >> 4) & 0x0F });
+        // Add the missing field
         fields.push({ name: "vps_max_num_reorder_pics", value: (data[9] >> 0) & 0x0F });
         fields.push({ name: "vps_max_latency_increase_plus1", value: data[10] });
         fields.push({ name: "vps_max_layer_id", value: data[11] });
-        fields.push({ name: "vps_max_dec_pic_buffering_minus1", value: (data[12] + (data[13] << 8)) >> 1 }); // Add the missing field
+        fields.push({ name: "vps_max_dec_pic_buffering_minus1", value: (data[12] + (data[13] << 8)) >> 1 });
     } else if (nalType === 33) {
         fields.push({ name: "sps_seq_parameter_set_id", value: data[5] & 0x1F });
         fields.push({ name: "sps_max_sub_layers_minus1", value: (data[6] >> 5) & 0x07 });
