2ad2316ebb512e7a2099ea7f8bc7efaec6cc4a83
Demo <demo@gmail.com>
2025-02-14 03:24:11 +0000

    Update data files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const version = 19
+const version = 20
 document.getElementById("version").innerText = version;
 let originalData = null;
 
@@ -61,6 +61,7 @@ function extractFields(nalType, data) {
         fields.push({ name: "sps_max_dec_pic_buffering_minus1", value: (data[9] & 0x0F) }); // Added max_dec_pic_buffering_minus1 field
         // Added sps_max_num_reorder_pics field
         fields.push({ name: "sps_max_num_reorder_pics", value: data[10] & 0x3F });
+        fields.push({ name: "sps_max_latency_increase_plus1", value: (data[10] >> 6) & 0x03 }); // Added max_latency_increase_plus1 field
     } else if (nalType === 34) {
         fields.push({ name: "pps_pic_parameter_set_id", value: data[5] & 0x3F });
         fields.push({ name: "pps_seq_parameter_set_id", value: data[6] & 0x1F });
@@ -88,6 +89,20 @@ document.getElementById("downloadBtn").addEventListener("click", function() {
     a.click();
 });
 
+// Updated code
 function modifyStream() {
-    return originalData || new Uint8Array([]);
+    if (!originalData) return;
+    let data = new Uint8Array(originalData); // Create a copy of the original data
+    for (let i = 0; i < data.length - 4; i++) {
+        if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 1) {
+            let nalType = (data[i + 3] & 0x7E) >> 1;
+            if (nalType === 33) {
+                // Update SPS
+                // ... Modify fields here ...
+                // For example:
+                data[i + 25] = 2; // Set sps_max_latency_increase_plus1 to 2
+            }
+        }
+    }
+    return data;
 }
